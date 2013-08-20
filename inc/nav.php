e960a560b7eb2da924c475dd934ae09dbf858e31
Kris <krivaten@gmail.com>
2013-08-20 11:51:21 -0400

    comment out unnecessary bootstrap files

--- inc/nav.php
+++ inc/nav.php
@@ -1,5 +1,4 @@
-<div class="navigation">
-	<div class="pull-left"><?php next_posts_link('<i class="icon-chevron-left"></i> Older Entries') ?></div>
-	<div class="pull-right"><?php previous_posts_link('Newer Entries <i class="icon-chevron-right"></i>') ?></div>
-	<div class="clearfix"></div>
-</div>
+<ul class="pager">
+	<li class="previous"><?php next_posts_link('<i class="icon-chevron-left"></i> Older Entries') ?></li>
+	<li class="next"><?php previous_posts_link('Newer Entries <i class="icon-chevron-right"></i>') ?></li>
+</ul>
--- js/js.js
+++ js/js.js
@@ -1,5 +1,11 @@
+//@prepros-append /bootstrap/alert.js
+//@prepros-append /bootstrap/button.js
+//@prepros-append /bootstrap/collapse.js
+//@prepros-append /bootstrap/dropdown.js
+//@prepros-append /bootstrap/modal.js
+//@prepros-append /bootstrap/tooltip.js
+//@prepros-append /bootstrap/transition.js
 
-//@prepros-append /bootstrap/bootstrap.min.js
-//@prepros-append /min/bootstrap-datepicker.min.js
-//@prepros-append /min/touchSwipe.min.js
-//@prepros-append /min/custom.min.js
+//@prepros-append /bootstrap-datepicker.js
+//@prepros-append /touchSwipe.js
+//@prepros-append /custom.js
