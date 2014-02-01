c424cc3a43a953e1a722bdbc3952cec8515a1f53
Kris Van Houten <krivaten@gmail.com>
2014-02-01 00:39:38 -0500

    minor changes to walkers to expand options

--- functions/walker-top-nav.php
+++ functions/walker-top-nav.php
@@ -14,7 +14,7 @@ class top_nav extends Walker_Nav_Menu {
 	 */
 	function start_lvl( &$output, $depth = 0, $args = array() ) {
 		$indent = str_repeat("\t", $depth);
-		$output .= "\n$indent<ul role=\"menu\" class=\" dropdown-menu\">\n";
+		$output .= "\n$indent<ul role=\"menu\" class=\" dropdown-menu pull-right\">\n";
 	}
 
 	/**
