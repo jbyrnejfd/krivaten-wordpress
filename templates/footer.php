70ac2e57a19f2a977a4f895d497e632358bcb73b
Kris Van Houten <krivaten@gmail.com>
2014-08-24 02:17:17 -0400

    add recent posts, address, and newsletts signup

--- templates/footer.php
+++ templates/footer.php
@@ -27,9 +27,31 @@
 				</div>
 				<div class="col-md-3 bg-danger">
 					<h4>recent posts</h4>
+					<ul class="list-unstyled">
+						<?php
+							$args = array('numberposts' => '5');
+							$recent_posts = wp_get_recent_posts($args);
+							foreach( $recent_posts as $recent ){
+								echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
+							}
+						?>
+					<li>
 				</div>
 				<div class="col-md-3 bg-warning">
 					<h4>the village church</h4>
+					<p>
+						210 s. second st. <br />
+						hamilton, oh <br />
+						513.330.9118
+					</p>
+
+					<p>sign up for our monthly email to stay up to date with <strong>the village church</strong>.</p>
+
+					<form action="http://myvillagechurch.us2.list-manage1.com/subscribe/post?u=5fb57d3da589671f6e1f6f346&amp;id=b836bd2f65" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" role="form" novalidate="">
+						<div class="form-group">
+							<input type="email" value="" name="EMAIL" id="mce-EMAIL" placeholder="email address" required="" class="form-control subscribeInput">
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
