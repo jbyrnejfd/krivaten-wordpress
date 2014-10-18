612272be05e7c2c7a8a1244b70cdaa065155765f
Kris Van Houten <krivaten@gmail.com>
2014-10-17 23:26:38 -0400

    udpates

--- templates/components/sub-pages.php
+++ templates/components/sub-pages.php
@@ -4,7 +4,6 @@
 		'container' => 'nav',
 		'container_class' => 'sidenav',
 		'menu_class' => 'nav',
-		'items_wrap' => '<div id="%1$s" class="%2$s">%3$s</div>',
 		'depth' => '0',
 		'walker' => new side_nav()
 	));
